ab99167d7bcb648e8c2cd9cfa023d3dab9112ff2
mtbstn24 <thushibstn24@gmail.com>
2023-03-29 18:10:43 +0530

    add endpoint to env

--- locustfile.py
+++ locustfile.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 apiKey = os.getenv('APIKEY')
+endPoint = os.getenv('ENDPOINT')
 
 class QuickstartUser(HttpUser):
     wait_time = between(5,9)
@@ -12,6 +13,6 @@ class QuickstartUser(HttpUser):
     def index(self):
         print("executing my_task")
         self.client.get(
-            url="/json", 
+            url=endPoint, 
             headers={"API-Key":apiKey},
         )
